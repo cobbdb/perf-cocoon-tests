73c976e55d8b7b1ad4be214b14bb91f27df4d1ce
Dan <cobbdb@gmail.com>
2015-07-21 00:43:38 -0700

    .

--- test.js
+++ test.js
@@ -2,22 +2,64 @@
 
 var suite = new Benchmark.Suite;
 
+var i, harness = [];
+for (i = 0; i < 100; i += 1) {
+    harness.push({
+        setname: function (name) {
+            this.name = name;
+        },
+        applysetname: function () {
+            this.setname.apply(this, arguments);
+        }
+    });
+}
+
 // add tests
-suite.add('RegExp#test', function() {
-  /o/.test('Hello World!');
-})
-.add('String#indexOf', function() {
-  'Hello World!'.indexOf('o') > -1;
-})
-.add('String#match', function() {
-  !!'Hello World!'.match(/o/);
-})
-// add listeners
-.on('cycle', function(event) {
-  console.log(String(event.target));
-})
-.on('complete', function() {
-  console.log('Fastest is ' + this.filter('fastest').pluck('name'));
-})
-// run async
-.run({ 'async': true });
+suite.
+    add('direct: for', function () {
+        var i, len = harness.length, pivot;
+        for (i = 0; i < len; i += 1) {
+            pivot = harness[i];
+            pivot.setname('bob');
+        }
+    }).
+    add('direct: for len-inner', function () {
+        var i, pivot;
+        for (i = 0; i < harness.length; i += 1) {
+            pivot = harness[i];
+            pivot.setname('bob');
+        }
+    }).
+    add('direct: forEach', function () {
+        harness.forEach(function (pivot) {
+            pivot.setname('bob');
+        });
+    }).
+    add('apply: for', function () {
+        var i, len = harness.length, pivot;
+        for (i = 0; i < len; i += 1) {
+            pivot = harness[i];
+            pivot.applysetname('bob');
+        }
+    }).
+    add('apply: for len-inner', function () {
+        var i, pivot;
+        for (i = 0; i < harness.length; i += 1) {
+            pivot = harness[i];
+            pivot.applysetname('bob');
+        }
+    }).
+    add('apply: forEach', function () {
+        harness.forEach(function (pivot) {
+            pivot.applysetname('bob');
+        });
+    }).
+    on('cycle', function (event) {
+        console.log(String(event.target));
+    }).
+    on('complete', function () {
+        console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+    }).
+    run({
+        async: true
+    });
