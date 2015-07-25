b28cee050e86e3b82fe6321dfc78f709a34e8771
Dan <cobbdb@gmail.com>
2015-07-25 02:36:41 -0700

    .

---
+++ tasks/exec.js
@@ -0,0 +1,9 @@
+﻿module.exports = function (grunt) {
+    grunt.config.merge({
+        exec: {
+            deploy: {
+                cmd: 'sftp harbinger "cd public_html/bin; put -o app.zip"'
+            }
+        }
+    });
+};
