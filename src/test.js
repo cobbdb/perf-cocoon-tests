3ec989d3343c7dcff36a28d2ba0fd52659b2edbc
Dan <cobbdb@gmail.com>
2015-07-31 00:33:14 -0700

    .

--- src/test.js
+++ src/test.js
@@ -1,86 +1,59 @@
 ﻿var Benchmark = require('benchmark'),
     suite = Benchmark.Suite();
 
-/**
- * TODO: Test is Dragon.PI is faster
- * than Math.PI
- */
-
-var i, j,
-    harness = [];
 function dosetup() {
-    harness = [];
-    for (i = 0; i < 1000; i += 1) {
-        if (i % 2 === 0) {
-            harness[i] = function () {};
-        } else {
-            harness[i] = [
-                function () {},
-                function () {}
-            ];
-        }
-    }
-}
+    var i,
+        data = {};
 
-function toArray(item) {
-    if (item) {
-        return item.push ? item : [item];
-    }
-    return [];
-}
-function concat() {
-    var i, len = arguments.length,
-        pivot, arr;
-    if (arguments[0].push) {
-        arr = arguments[0];
-        i = 1;
-    } else {
-        arr = [];
-        i = 0;
-    }
-    for (i; i < len; i += 1) {
-        pivot = arguments[i];
-        if (pivot || pivot === 0 || pivot === '') {
-            if (pivot.push) {
-                arr.push.apply(arr, pivot);
-            } else {
-                arr.push(pivot);
-            }
+    function forIn(root, map) {
+        var i,
+            keys = Object.keys(root),
+            len = keys.length;
+        for (i = 0; i < len; i += 1) {
+            map(keys[i]);
         }
     }
-    return arr;
+
+    for (i = 0; i < 20; i += 1) {
+        data[i] = '' + i;
+    }
 }
 
 // add tests
 suite.
-    add('concat', {
+    add('for.. in', {
         setup: dosetup,
+        minSamples: 100,
         fn: function () {
-            var i, len = harness.length, pivot, temp;
-            for (i = 0; i < len; i += 1) {
-                pivot = harness[i];
-                temp = [].concat(pivot, pivot);
+            var key, temp;
+            for (key in data) {
+                temp = data[key];
             }
         }
     }).
-    add('detect', {
+    add('for keys()', {
         setup: dosetup,
+        minSamples: 100,
         fn: function () {
-            var i, len = harness.length, pivot, temp;
+            var key, keys, len, i, temp;
+
+            keys = Object.keys(data);
+            len = keys.length;
+
             for (i = 0; i < len; i += 1) {
-                pivot = harness[i];
-                temp = toArray(pivot);
+                key = keys[i];
+                temp = data[key];
             }
         }
     }).
-    add('manual', {
+    add('manual forIn', {
         setup: dosetup,
+        minSamples: 100,
         fn: function () {
-            var i, len = harness.length, pivot, temp;
-            for (i = 0; i < len; i += 1) {
-                pivot = harness[i];
-                temp = concat(pivot, pivot);
-            }
+            var temp;
+            forIn(data, function (key) {
+                temp = data[key];
+            });
         }
     }).
     on('cycle', function (event) {
